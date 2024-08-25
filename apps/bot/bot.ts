a6bad06b2ecd662d71887bdb403179449cabf66a
noisyboy25 <dj402and@gmail.com>
2024-08-25 20:42:36 +0300

    add menu for product creation

--- apps/bot/bot.ts
+++ apps/bot/bot.ts
@@ -1,9 +1,19 @@
-import 'dotenv/config';
-import { Bot, InlineKeyboard } from 'grammy';
+import dotenvx from '@dotenvx/dotenvx';
+dotenvx.config();
+import { Bot, Context, InlineKeyboard, session } from 'grammy';
 import express from 'express';
-import { Order } from '@tg-shop-pg/common';
+import { Order, Product } from '@tg-shop-pg/common';
+import { Menu } from '@grammyjs/menu';
+import {
+  Conversation,
+  createConversation,
+  ConversationFlavor,
+  conversations,
+} from '@grammyjs/conversations';
 
 const IMAGE_API = process.env.IMAGE_API;
+
+let MINIAPP_URL = '';
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 if (!TELEGRAM_TOKEN) {
@@ -17,36 +27,7 @@ if (!CHANNEL_ID) {
   process.exit(1);
 }
 
-const bot = new Bot(TELEGRAM_TOKEN);
-
-bot.catch((err) => {
-  console.log({ err });
-});
-
-bot.command('start', async (ctx) => {
-  const botUsername = (await ctx.api.getMe()).username;
-  await ctx.reply('Open App', {
-    reply_markup: new InlineKeyboard().url(
-      'Open App',
-      `https://t.me/${botUsername}?startapp`
-    ),
-  });
-});
-
-bot.start();
-
-const app = express();
-app.use(express.json());
-
-const api = express.Router();
-app.use('/api', api);
-
-api.get('/', (req, res) => {
-  res.json({ message: `Hello from API (${Date.now()})` });
-});
-
-const productApi = express.Router();
-api.use('/products', productApi);
+const productList: Product[] = [];
 
 const getImages = async (limit = 15) => {
   const res = await fetch(
@@ -57,12 +38,6 @@ const getImages = async (limit = 15) => {
   return data.items;
 };
 
-const productList: {
-  id: string;
-  name: string;
-  price: number;
-  image: string;
-}[] = [];
 const generateProducts = async () => {
   try {
     const images = await getImages();
@@ -83,6 +58,80 @@ const generateProducts = async () => {
 };
 generateProducts();
 
+type MyContext = Context & ConversationFlavor;
+
+const bot = new Bot<MyContext>(TELEGRAM_TOKEN);
+
+bot.catch((err) => {
+  console.log({ err });
+});
+
+bot.use(session({ initial: () => ({}) }));
+bot.use(conversations());
+
+const adminMenu = new Menu<MyContext>('admin-menu').submenu(
+  'Products',
+  'products-menu'
+);
+
+type ProductFormConversation = Conversation<MyContext>;
+
+const productForm = async (
+  conversation: ProductFormConversation,
+  ctx: MyContext
+) => {
+  await ctx.reply('Product name:');
+  const { message } = await conversation.wait();
+  if (!message?.text) return;
+  await addProduct(message.text);
+  await ctx.reply(`Product name is ${message.text}`);
+};
+bot.use(createConversation(productForm));
+
+const productsMenu = new Menu<MyContext>('products-menu')
+  .text('Add product', async (ctx) => {
+    await ctx.conversation.enter('productForm');
+  })
+  .back('Back');
+
+adminMenu.register(productsMenu);
+bot.use(adminMenu);
+
+bot.command('start', async (ctx) => {
+  await ctx.reply('Open App', {
+    reply_markup: new InlineKeyboard().url('Open App', MINIAPP_URL),
+  });
+});
+
+bot.command('admin', async (ctx) => {
+  await ctx.reply('Admin menu', { reply_markup: adminMenu });
+});
+
+bot.command('cancel', async (ctx) => {
+  await ctx.conversation.exit();
+  await ctx.reply('Cancelled');
+});
+
+bot.start({
+  onStart: (botInfo) => {
+    console.log(botInfo);
+    MINIAPP_URL = `https://t.me/${botInfo.username}?startapp`;
+  },
+});
+
+const app = express();
+app.use(express.json());
+
+const api = express.Router();
+app.use('/api', api);
+
+api.get('/', (req, res) => {
+  res.json({ message: `Hello from API (${Date.now()})` });
+});
+
+const productApi = express.Router();
+api.use('/products', productApi);
+
 productApi.get('/', (req, res) => {
   res.json({
     products: productList,
@@ -92,7 +141,6 @@ productApi.get('/', (req, res) => {
 api.route('/orders').post(async (req, res) => {
   const order: Order = req.body.order;
   order.id = `${Date.now()}`;
-  const { id } = order;
   console.dir(order, { depth: null });
   res.json({ message: 'created order', order });
   const formattedCart = order.cart
@@ -101,7 +149,7 @@ api.route('/orders').post(async (req, res) => {
         `[${product.id}] (x ${quantity}) ${product.name}  `
     )
     .join('\n');
-  const formattedOrder = `<b>ID:</b> ${id}<b>\n<b>Phone:</b> ${order.customer.phone}\n<b>Name:</b> ${order.customer.name}\nCart:</b>\n${formattedCart}`;
+  const formattedOrder = `<b>ID:</b> ${order.id}<b>\n<b>Phone:</b> ${order.customer.phone}\n<b>Name:</b> ${order.customer.name}\nCart:</b>\n${formattedCart}`;
   try {
     await bot.api.sendMessage(CHANNEL_ID, formattedOrder, {
       parse_mode: 'HTML',
@@ -110,6 +158,16 @@ api.route('/orders').post(async (req, res) => {
     console.error(error);
   }
 });
+
+const addProduct = async (name: string, price = 1, image = '') => {
+  const product: Product = {
+    id: `${Date.now()}`,
+    name,
+    price,
+    image,
+  };
+  productList.push(product);
+};
 
 const PORT = process.env.PORT ?? 5000;
 try {
