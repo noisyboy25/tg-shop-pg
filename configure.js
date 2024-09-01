fc6dbac85830a6f6d0cbbbcadacca1ac57bdfc29
noisyboy25 <dj402and@gmail.com>
2024-09-01 06:58:20 +0300

    hide token input

--- configure.js
+++ configure.js
@@ -1,30 +1,27 @@
-import { createInterface } from 'node:readline/promises';
-import fs from 'fs/promises';
+import rls from 'readline-sync';
+import fs from 'fs';
 import path from 'path';
 
 const CONFIG_PATH = path.resolve('./apps/bot/.env');
 
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const telegramToken = rls.question('Telegram token: ', { hideEchoBack: true });
+if (!telegramToken) {
+  console.error('ERROR: Token not specified');
+  process.exit(1);
+}
+const channelId = rls.question('Channel ID: ');
+if (!channelId) {
+  console.error('ERROR: Channel ID not specified');
+  process.exit(1);
+}
 
-const main = async () => {
-  const telegramToken = await rl.question('Telegram token: ');
-  if (!telegramToken) return console.error('ERROR: Token not specified');
-  const channelId = await rl.question('Channel ID: ');
-  if (!channelId) return console.error('ERROR: Channel ID not specified');
-  rl.close();
-
-  await fs.writeFile(
-    CONFIG_PATH,
-    `SHOP_URL=http://localhost:5173
+fs.writeFileSync(
+  CONFIG_PATH,
+  `SHOP_URL=http://localhost:5173
 PORT=5000
 IMAGE_API=https://api.nekosapi.com/v3
 TELEGRAM_TOKEN=${telegramToken}
 CHANNEL_ID=${channelId}
 `
-  );
-  console.log(`Config file saved to ${CONFIG_PATH}`);
-};
-main();
+);
+console.log(`Config file saved to ${CONFIG_PATH}`);
