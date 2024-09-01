e95dbda75d6b6f1bf21cc2b9f4d643a4f0b6965d
noisyboy25 <dj402and@gmail.com>
2024-09-01 05:53:31 +0300

    update configuration script

--- configure.js
+++ configure.js
@@ -11,12 +11,19 @@ const rl = createInterface({
 
 const main = async () => {
   const telegramToken = await rl.question('Telegram token: ');
+  if (!telegramToken) return console.error('ERROR: Token not specified');
   const channelId = await rl.question('Channel ID: ');
+  if (!channelId) return console.error('ERROR: Channel ID not specified');
   rl.close();
 
   await fs.writeFile(
     CONFIG_PATH,
-    `TELEGRAM_TOKEN=${telegramToken}\nCHANNEL_ID=${channelId}\n`
+    `SHOP_URL=http://localhost:5173
+PORT=5000
+IMAGE_API=https://api.nekosapi.com/v3
+TELEGRAM_TOKEN=${telegramToken}
+CHANNEL_ID=${channelId}
+`
   );
   console.log(`Config file saved to ${CONFIG_PATH}`);
 };
